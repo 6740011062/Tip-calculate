a3e8e26104c11b18ce9ceefd1d71bee88fd0d8c6
apisra <6740011062@psu.ac.th>
2026-02-13 14:23:39 +0700

    Up

---
+++ app/page.tsx
@@ -0,0 +1,32 @@
+"use client"
+import { useState } from "react"
+
+const IndexPage = () =>{
+  const [todo, setTodo] = useState('')
+  const [todos,setTodos] = useState([
+    {id: 1, content: 'Todo1'},
+    {id: 2, content: 'Todo2'},
+    {id: 3, content: 'Todo3'},
+  ])
+
+  const handleAddFormChanged = (event) => {
+    setTodo(event.target.value)
+  }
+  const addTodo = () =>{
+    setTodos([{id: todos.length +1, content: todo}, ...todos])
+  }
+
+  return(
+    <>
+    <div>{todo}</div>
+      <input type="text" onChange={handleAddFormChanged}/>
+      <button onClick={addTodo}>Add</button>
+      <ul>
+        {todos.map(todo => <li key={todo.id}>{todo.content}</li>)}
+      </ul>
+    </>
+  )
+  
+
+}
+export default IndexPage
